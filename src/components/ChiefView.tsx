90a50dd2a8a3867dfce528bfad3cee38d70a87e2
PILN <matveys909@gmail.com>
2026-03-01 11:10:29 +0300

    feat/fix: shtab nachalnic

--- src/components/ChiefView.tsx
+++ src/components/ChiefView.tsx
@@ -4,9 +4,9 @@ import type { FireSimState } from '../types/firesim';
 export default function ChiefView({ simState }: { simState: FireSimState | null }) {
   return (
     <FireMapView
-      dataPrefix="/firemaps"
-      equipmentEndpoint="/game_logics/car"
-      hoseEndpoint="/game_logics/hose"
+      dataPrefix="/headquarters"
+      equipmentEndpoint="/hq_game_logic/car"
+      hoseEndpoint="/hq_game_logic/hose"
       simState={simState}
     />
   );
